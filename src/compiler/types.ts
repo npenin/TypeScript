142b502f5474d7ae73256fe8f5019843d10e16aa
Sheetal Nandi <shkamat@microsoft.com>
2022-10-06 10:41:12 -0700

    Add plugin override for watchFactory

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -6685,6 +6685,7 @@ namespace ts {
     /*@internal*/
     export interface WatchOptionsFactoryHost {
         searchPaths: readonly string[];
+        getPluginWithConfigOverride(plugin: PluginImport): PluginImport;
     }
     export interface WatchOptions {
         watchFile?: WatchFileKind;
